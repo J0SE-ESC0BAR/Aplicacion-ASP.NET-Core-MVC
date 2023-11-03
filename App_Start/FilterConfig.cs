1e30b30fc9fa6d4f7e17b437e32871ddddeab85a
José Alfonso Escobar Mejía <josealfonsoescobarmejia@gmail.com>
2023-11-03 06:26:18 -0600

    Proyecto recién creado
    Proyecto recién creado en Aplicacion web ASP.NET (.NET Framework 4.7.2) con la plantilla MVC

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_Photos_Adventure_Works
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto_Photos_Adventure_Works.MvcApplication" Language="C#" %>
